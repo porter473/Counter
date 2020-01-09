b6bb837b49793118d57e9d197dde0d0dff71a68e
Aman Kumar Singh <aman.singh@theporter.in>
2020-01-09 19:11:04 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {timer: 0, intervalId: null, startButton: true, pauseButton: false, resetButton: false};
+  }
+  componentDidMount()
+  {
+
+  }
+  handleStart= (e) => { 
+    this.setState({intervalId: setInterval(()=>{
+      this.setState({timer: this.state.timer+1});
+    },1000) 
+  }) 
+  this.setState({startButton: false, pauseButton: true, resetButton: true})
+}
+
+  handlePause= (e) => {
+    clearInterval(this.state.intervalId);
+
+  this.setState({startButton: true, pauseButton: false, resetButton: true})
+  }
+  handleReset= (e) => {
+    clearInterval(this.state.intervalId);
+    this.setState({timer: 0,startButton: true, pauseButton: false, resetButton: false});
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Counter</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.timer}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="Buttons">
+          <button id="start" disabled={!this.state.startButton} onClick={(e)=>this.handleStart(e)}>
+            Start
+          </button>
+          <button id="pause" disabled={!this.state.pauseButton} onClick={(e)=>this.handlePause(e)}>
+            Pause
+          </button>
+          <button id="reset" disabled={!this.state.resetButton} onClick={(e)=>this.handleReset(e)}>
+            Reset
+          </button>
+          
+        </div>
+        
+
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
